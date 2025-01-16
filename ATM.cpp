6c621c7528f84f45af7209c63c31a1dac058aa6f
Tanzeelp <tanzeelmd71@gmail.com>
2025-01-16 17:39:30 +0530

    Final project is launched

--- ATM.cpp
+++ ATM.cpp
@@ -9,32 +9,39 @@ class info
     {
         cout<<endl<<"Enter the ID :";
         cin>>id;
-        cout<<endl<<balance;
-
-        cout<<endl<<"Select One Option";
+        cout<<endl<<"Balance : "<<balance;
+        cout<<endl<<"1 : Check Balance";
+        cout<<endl<<"2 : Withdraw Money";
+        cout<<endl<<"3 : Deposit Money";
+        cout<<endl<<"Select One Option : ";
+        cin>>o;
         switch(o)
         {
             case 1:
-            cout<<endl<<"Check Balance"<<balance;
+            cout<<endl<<"Check Balance : "<<balance;
             break;
 
             case 2:
-            cout<<endl<<"Withdraw Money";
+            cout<<endl<<"Withdraw Money : "<<endl<<"Enter the amount to withdraw : ";
+            cin>>withdraw;
+
             if(balance>=withdraw)
             {
                 balance=balance-withdraw;
-                cout<<endl<<id<<endl<<balance;
+                cout<<endl<<"Balance :"<<balance;
             }
             else
-            cout<<endl<<"Enter sufficient amount"<<balance;
+            cout<<endl<<"Enter sufficient amount"<<endl<<"Balance : "<<balance;
             break;
 
             case 3:
-            cout<<endl<<"Deposit Money";
+            cout<<endl<<"Deposit Money : ";
+            cout<<endl<<"Enter the amount to deposit : ";
+            cin>>deposit;
             if(deposit>0)
             {
-                deposit=balance+deposit;
-                cout<<endl<<id<<endl<<balance;
+                balance=balance+deposit;
+                cout<<endl<<"Balance :"<<balance;
             }
             break;
         }
